1ed8aa78563d1738315c10151676a0b8c6348ab1
Glitch (whoami-fcc-amirzamzam) <none>
2018-03-26 08:41:34 +0000

    🕌🐂 Checkpoint ./server.js:780437/963

--- server.js
+++ server.js
@@ -9,38 +9,17 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
-
-
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'))
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + '/views/index.html')
-})
+var api = '/api/whoami';
 
-// Simple in-memory store
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-]
+app.get(api, function(req, res, next){
+var language;
+var software;
+var
+});
 
-app.get("/dreams", (request, response) => {
-  response.send(dreams)
-})
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", (request, response) => {
-  dreams.push(request.query.dream)
-  response.sendStatus(200)
-})
-
-// listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log(`Your app is listening on port ${listener.address().port}`)
 })
