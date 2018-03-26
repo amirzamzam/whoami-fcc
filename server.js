dcff1752a1ea5b9d89efb4604483f428e514ff6d
Glitch (whoami-fcc-amirzamzam) <none>
2018-03-26 08:19:33 +0000

    🏣📟 Checkpoint ./server.js:780437/121

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 // where your node app starts
 
 // init project
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
